d84f1b0672fb8dae8b1a96f9268cdb8e0bc5c706
ANA KHASANAH <ana.khasanah@capita.com>
2023-03-13 21:19:19 +0000

    Add Circle Template

--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -1,9 +1,9 @@
 public class CircleCalculator {
     public static void main(String[] args) {
         // 1. declare variable called circleRadius, assign value of 12.0
-
+                double circleRadius = 12.0;
         // 2. print the circleRadius
-        
+
         // 3. Create variable circleDiameter, calculate using 2 * circleRadius
 
         // 4. Print variable circleDiameter, e.g "The circle diameter  is " + circleDiameter
@@ -11,7 +11,7 @@ public class CircleCalculator {
         // 5. Create variable circleCircumference, calculate using 2 * 3.14 * circleRadius
 
         // 6. Print variable circleCircumference
-        
+
         // 7. Create variable circleArea, calculate using  3.14 * circleRadius * circleRadius
 
 
